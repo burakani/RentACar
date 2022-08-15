1a0d484e6743dabc4393d02cfadb732857ac2aee
burakani <burakani52@gmail.com>
2022-08-16 02:55:53 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,32 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Car brand
+    /// </summary>
+    public class Brand : Entity
+    {
+        public Brand()
+        {
+
+        }
+
+        public Brand(int id, string name) : this()
+        {
+            Id = id;
+            Name = name;
+        }
+        
+        /// <summary>
+        /// Brand name
+        /// </summary>
+        public string Name { get; set; }
+
+    }
+}
